5a5695c7fb9b902ba22c7859b6101a147572c499
Mirzo-Ulugbek Fazilov <mirzoulugbekfazilov@gmail.com>
2026-04-15 03:03:51 +0100

    Update app.run to use debug mode and specific host

--- app.py
+++ app.py
@@ -2057,5 +2057,6 @@ def logout():
 
 # ------------------ Run ------------------
 if __name__ == "__main__":
+    app.run(debug=True, host="127.0.0.1", port=8000)
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
